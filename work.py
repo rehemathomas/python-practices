cf5bd6bc2006b3dc6584c348f4d8c5dee0de055c
rehemathomas <thomasrey850@gmail.com>
2025-09-30 14:42:56 +0300

    printing repeated message

--- work.py
+++ work.py
@@ -2,3 +2,8 @@
 # Printing  
 print("Rehema Thomas Mtavangu")
 print("My dream is to become a professional Website developer and IT specialist in IoT and AI/ML expert")
+
+# Printing the same message 3 times 
+print("I love coding")
+print("I love coding") 
+print("I love coding")
